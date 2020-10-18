2e79b92342214a1aa40703516df16d6fbbc74f56
Samridh212 <70936587+Samridh212@users.noreply.github.com>
2020-10-18 17:52:48 +0530

    Add files via upload

---
+++ Block.js
@@ -0,0 +1,28 @@
+class Box {
+  constructor(x, y, width, height) {
+    var options = {
+        isStatic:false,
+        'restitution':0.5,
+        'friction':0.5,
+        'density':0.5
+
+    }
+    this.body = Matter.Bodies.rectangle(x, y, width, height, options);
+    this.width = width;
+    this.height = height;
+    this.x = x;
+    this.y = y;
+
+    World.add(world, this.body);
+  }
+  display(){
+    var pos = this.body.position;
+    push();
+    translate(pos.x, pos.y);
+    rectMode(CENTER);
+    strokeWeight(1);
+    stroke(0);
+    rect(0,0,this.width,this.height);
+    pop();
+  }
+}
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# Pro-29
+# MatterJSBoilerPlate
+MatterJSBoilerPlate
